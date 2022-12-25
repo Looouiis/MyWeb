09b4e40b6689a15da532469d3d964982771d0a42
Looouiiis <Looouiiis@126.com>
2022-12-25 22:28:38 +0800

    补交文件

---
+++ src/main/webapp/frontend/src/components/Markdown.vue
@@ -0,0 +1,64 @@
+<script>
+import { marked } from 'marked'
+import { debounce } from 'lodash-es'
+
+export default {
+  name: 'Markdown',
+  data: () => ({
+    input: "**简陋介绍一下markdown语法**<br><br>“#”+一个空格开头表示为**标题**的特殊符号，**n个“#”代表n级标题**<br><br>“\*\*”+您的内容+“\*\*”会将“您的内容”标为**粗体**；&emsp;“\*”+您的内容+“\*”会将您的内容标为*斜体*；&emsp;“\~”+您的内容+“\~”则为~删除线~<br><br>在**新的段落开头**输入“*”+一个空格表示无编号要点<br>在**新的段落开头**输入数字+“.”+一个空格表示有编号要点（输入框中另起一行且新行开头**不超过三个空格**则会自动纠正为顺编号的顺序，超过三个空格表示为子要点（具体效果可前往**typora**等专业markdown软件中查看））"
+  }),
+  computed: {
+    output() {
+      return marked(this.input)
+    }
+  },
+  methods: {
+    update: debounce(function (e) {
+      this.input = e.target.value
+    }, 100)
+  }
+}
+</script>
+
+<template>
+  <div class="editor">
+    <textarea class="input" :value="input" @input="update"></textarea>
+    <div class="output" v-html="output"></div>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+.editor {
+  height: 100vh;
+  display: flex;
+}
+
+.input,
+.output {
+  overflow: auto;
+  width: 50%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+  text-align: left;
+}
+
+.input {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
+</style>
---
+++ src/main/webapp/frontend/src/views/Message.vue
@@ -0,0 +1,33 @@
+<!-- <template>
+  
+</template>
+<script>
+export default {
+  name: 'Message'
+  
+}
+</script> -->
+<script>
+import TextHandler from '@/components/TextHandler.vue'
+
+export default {
+  name: 'Message',
+  components:{
+    TextHandler
+  },
+  data: () => ({
+    input: ''
+  }),
+  methods: {
+    
+  }
+}
+</script>
+
+<template>
+  <TextHandler/>
+</template>
+
+<style>
+
+</style>
